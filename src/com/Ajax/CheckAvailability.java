54f818f3becae161cb2a40da12e872f4d214cd2f
pradeep tripathi <pk.tripathi700@gmail.com>
2015-12-21 23:30:58 +0530

    second commit

--- src/com/Ajax/CheckAvailability.java
+++ src/com/Ajax/CheckAvailability.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class CheckAvailability
  */
+//author pradeep
 @WebServlet("/CheckAvailability")
 public class CheckAvailability extends HttpServlet {
 	private static final long serialVersionUID = 1L;
